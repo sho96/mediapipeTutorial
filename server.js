ab883124b9d3d8b1eb16024ad36c5d9844f8878a
sho96 <81459356+sho96@users.noreply.github.com>
2023-11-29 15:34:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const { readFileSync } = require("fs");
-const http = require("http");
-const app = express();
-const httpPort = process.env.PORT || 8000;
+const express = require("express"); //expressモジュールをインポート（コードの中で使えるようにする）
+const { readFileSync } = require("fs"); //fsモジュールの中からreadFileSync関数をインポート
+const http = require("http"); //httpモジュールをインポート
+const app = express(); //expressモジュールを使ってサーバーの骨組みを作る
+const httpPort = 8000; //httpPort という名前の変数に 8000 という値を代入
 
+//サーバーのメインルート ("/") にアクセスするとindex.htmlファイルの内容をユーザーに提供
+//サーバーのルートとは,
+// google.com/search の /search の部分や、
+// github.com/sho96/mediapipeTutorial の /sho96/mediapipeTutorial の部分のことである。
 app.get("/", (req, resp) => {
-  resp.status(200).send(readFileSync("./index.html"))
+  resp.status(200).send(readFileSync("./index.html"));
 });
 
+//作ったサーバーの骨組みから、ポート番号をhttpPort (今回は8000) でサーバーを公開する。
 httpServer = http.createServer(app);
 httpServer.listen(httpPort);
+
+console.log(`server listening on port ${httpPort}`);
